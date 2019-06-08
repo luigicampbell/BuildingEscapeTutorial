35dec5bfcf50359b1faa7b204552b4d90e56ec03
Luigi Campbell <luigi.campbell@outlook.com>
2019-06-08 14:01:21 -0700

    getting position

--- BuildingEscape/Source/BuildingEscape/PositionReport.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -22,7 +22,8 @@ void UPositionReport::BeginPlay()
 	Super::BeginPlay();
 	// ...
   FString ObjectName = GetOwner() -> GetName();
-  UE_LOG(LogTemp, Warning, TEXT("Position Report for %s"), *ObjectName);
+  FString ObjectPosition = GetOwner() -> GetTransform().GetLocation().ToString();
+  UE_LOG(LogTemp, Warning, TEXT("Position Report for %s"), *ObjectName, *ObjectPosition);
 	
 }
 
